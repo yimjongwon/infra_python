514e8a8631562179ca731f37b52edebb03e0ed99
yimjongwon <awosung00@naver.com>
2026-03-27 18:13:35 +0900

    0327수업정리

--- my_memo.py
+++ my_memo.py
@@ -78,5 +78,34 @@ systemctl  disable  firewalld  # 재부팅 했을때도 내려 가도록
 
 import os
 
+# yum 페키지 메니저 업데이트 (Linux 설치후 최초 한번만 하면 된다)
+yum update -y
+# 만일 python 이 설치 되어 있지 않으면 (rocky linux 는 이미 설치 되어 있다)
+yum install -y python3 
+# python 가상 환경 생성
+python3  -m  venv  venv
+# 가상환경 활성화
+source  venv/bin/activate
+# python 페키지 메니저 update
+pip  install  --upgrade  pip
+# 의존 페키지 설치
+pip  install  fastapi  uvicorn  jinja2
+# 의존 페키지  freeze
+pip  freeze  >  requirements.txt
+# fastapi  서버실행
+uvicorn  main:app  --host  0.0.0.0  --port  8000
 
+
+
+**vm1 or ec2 로그인** 정리: ***서버*에는 .pem의 개인키, *로컬*에는 공개키의 구조**
+
+```plaintext
+[root@testvm1 /]# cat test_key.pem.pub
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDC3h50Nki/sS4lzSFamDBNz8SRQulZkIjIZt5KOXsK6wNxn9u9n2SZY+gYBUGiugI7jU1usvSs6s90kBaeO8BQYJSQaXrj+b4Oi7YM0Fll/A9HzD6SjUOYeuQUmMNlBCHtuQpztewTOU4M9lTI873HV6NE6vXJwtUUvimVaDOUS7d+txQ6l9XDKinZlW2dszLJOEt2tR9poTH1asLZFq1IORQkskgroSYFK0NdjvE3uLuj7nt9zK94RDHKUAB55u8K3hU0Vn85ytNgSaEvj3AOgdi4jKhMsvt/dcMDnOjuYla9z+AICvx0pvt/8dcnWrKja7aIXoo+ABuJ+dnEZZyx9oYBb40iNmHtZEtIIhspbD/zRZxfQlGerIvI02QZnzu5SSWOwSbpmvc8up0a+HDbWSDpGTVvkD/MxpTqxuqkd8dWLhO4QJvOL6/nVQlhgqImWrB/aQyMHfJkkQDzKGypQr9dnsYsCyQlIMHrz+slZjnhNfu3x2vhOkC2s5aq9ac= root@testvm1
+[root@testvm1 /]# cat test_key.pem.pub >> ~/.ssh/authorized_keys
+[root@testvm1 ~]# chmod 600 ~/.ssh/authorized_keys:권한 낮추기
+ssh-keygen -q -N "" -f test_key.pem: 공개키, 개인키 두개 생성 
+-rw-------    1 root root 2602 Mar 27 15:10 test_key.pem
+-rw-r--r--    1 root root  566 Mar 27 15:10 test_key.pem.pub
+```
 '''
